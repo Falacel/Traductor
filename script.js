eda48e404fe5a168ef1e2442a0b2a1867701c3d1
Andrés <andres_1002@hotmail.com>
2025-08-19 13:05:29 +0200

    Script con API

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-const dictionary = {
-  "hola": "hello",
-  "adiós": "goodbye",
-  "gracias": "thank you",
-  "por favor": "please",
-  "perro": "dog",
-  "gato": "cat",
-  "hello": "hola",
-  "goodbye": "adiós",
-  "thank you": "gracias",
-  "please": "por favor",
-  "dog": "perro",
-  "cat": "gato"
-};
+async function translateText() {
+    let text = document.getElementById("inputText").value
+    let from = document.getElementById("fromLang").value
+    let to = document.getElementById("toLang").value
 
-function translateText() {
-  let text = document.getElementById("inputText").value.toLowerCase().trim();
-  let translation = dictionary[text] || "No encontrado en el diccionario";
-  document.getElementById("outputText").innerText = translation;
+    if(!text) {
+        document.getElementById("outputText").innerText = "⚠️ Escribe algo para traducir."
+        return
+    }
+
+    try{
+        let response = await fetch("https://libretranslate.de/translate",{
+            method: "POST",
+            headers: {"Content-Type": "application/json" },
+            body: JSON.stringify({
+                q: text,
+                source: from,
+                target: to,
+                format: "text"
+            })
+        })
+        
+    let data = await response.json();
+        document.getElementById("outputText").innerText = data.translatedText;
+    } catch (error) {
+        document.getElementById("outputText").innerText = "❌ Error al traducir";
+        console.error(error);
+    }
 }
